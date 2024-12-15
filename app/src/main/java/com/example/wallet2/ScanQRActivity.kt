43f72d7f3b2240a89630a853bb964a7c3d8bf462
TuongDuyHuong <khongphaihuong2k4@gmail.com>
2024-12-15 17:08:50 +0700

    wallet project

---
+++ app/src/main/java/com/example/wallet2/ScanQRActivity.kt
@@ -0,0 +1,11 @@
+package com.example.wallet2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScanQRActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan_qractivity)
+    }
+}
